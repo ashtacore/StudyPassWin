a265e81c488dece1627f542f4be389c1f456f214
Joshua Runyan <runyan@ashtacore.com>
2025-10-12 22:13:50 -0700

    consistent style

--- src/SignOutButton.tsx
+++ src/SignOutButton.tsx
@@ -12,7 +12,7 @@ export function SignOutButton() {
 
   return (
     <button
-      className="px-4 py-2 rounded bg-white dark:bg-gray-700 text-secondary dark:text-gray-300 border border-gray-200 dark:border-gray-600 font-semibold hover:bg-gray-50 dark:hover:bg-gray-600 hover:text-secondary-hover dark:hover:text-white transition-colors shadow-sm hover:shadow"
+      className="px-3 md:px-4 py-2 rounded-lg bg-white dark:bg-gray-700 text-secondary dark:text-gray-300 border border-gray-200 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-600 hover:text-secondary-hover dark:hover:text-white  transition-colors text-xs md:text-sm font-medium whitespace-nowrap shadow-sm hover:shadow"
       onClick={() => void signOut()}
     >
       Sign out
